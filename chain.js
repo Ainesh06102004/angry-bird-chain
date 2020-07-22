a8afc24a36164da008f946508798fc6d43387337
Ainesh06102004 <aineshnanda@gmail.com>
2020-07-22 17:48:59 +0530

    chains

---
+++ chain.js
@@ -0,0 +1,21 @@
+class Chain {
+   constructor(bodyA, bodyB) {
+       var options = {
+           bodyA: bodyA,
+           bodyB : bodyB,
+           stiffness : 0.01,
+           length: 15
+       }
+       this.chain = Constraint.create(options);
+       World.add(world, this.chain);
+   }
+
+   display(){
+       var positionA = this.chain.bodyA.position;
+       var positionB = this.chain.bodyB.position;
+ 
+       strokeWeight(4);
+       line(positionA.x , positionA.y , positionB.x, positionB.y); 
+
+   }
+}
